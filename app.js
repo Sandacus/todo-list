6f5892858a71871f03b8eba93e97afe6f27adbc3
Sandy Lamond <sandyarl329@gmail.com>
2023-04-22 22:39:33 +0100

    Added the ejs engine to app.js

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use("view engine", "ejs");
 
 app.get("/", function(req, res){
     // res.send("Hello");
