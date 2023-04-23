d4285ff586302c3e41667105d4ae912c6ca1fab7
Sandy Lamond <sandyarl329@gmail.com>
2023-04-23 22:36:14 +0100

    Added an updating list for todo items using the scriptlet ejs tags so the todo dynamically adds news items.

--- app.js
+++ app.js
@@ -4,26 +4,25 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+let listItems = [];
+
 app.set("view engine", "ejs");
 
-let weekDays = {
-    0: "Sunday", 
-    1: "Monday", 
-    2: "Tuesday",
-    3: "Wednesday",
-    4: "Thursday",
-    5: "Friday",
-    6: "Saturday"};
+app.use(bodyParser.urlencoded({extended: true}));
+
+
 
 app.get("/", function(req, res){
-
     let today = new Date();
-    let currentDay = today.getDay();
     let options = { weekday: "long", year: "numeric", month: "long", day: "numeric" }
     let day = today.toLocaleDateString("en-GB", options)
+    res.render("index", { kindOfDay: day, newItems: listItems });
+});
 
-    res.render("index", {kindOfDay: day});  // weekDays[currentDay]
-
+app.post("/", function(req, res){
+    listItem = req.body.newItem;
+    listItems.push(listItem);
+    res.redirect("/");
 });
 
 app.listen(3000, function(){
