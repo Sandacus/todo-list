4df8c3809ae27d342e46a6275357ab6a5f211a95
Sandy Lamond <sandyarl329@gmail.com>
2023-04-30 21:17:53 +0100

    Added header and footer ejs template sections and an about page.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 let listItems = [];
+let workItems = [];
 
 app.set("view engine", "ejs");
 
@@ -16,15 +17,35 @@ app.get("/", function(req, res){
     let today = new Date();
     let options = { weekday: "long", year: "numeric", month: "long", day: "numeric" }
     let day = today.toLocaleDateString("en-GB", options)
-    res.render("index", { kindOfDay: day, newItems: listItems });
+    res.render("list", { listTitle: day, newItems: listItems });
 });
 
 app.post("/", function(req, res){
-    listItem = req.body.newItem;
-    listItems.push(listItem);
+    let item = req.body.newItem;
+
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    }
+
+    listItems.push(item);
     res.redirect("/");
 });
 
 app.listen(3000, function(){
     console.log("Server started on port 3000");
+});
+
+app.get("/work", function(req, res){
+    res.render("list", {listTitle: "Work List", newItems: workItems});
+});
+
+app.post("/work", function(req, res){
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+})
+
+app.get("/about", function(req, res) {
+    res.render("about");
 });
